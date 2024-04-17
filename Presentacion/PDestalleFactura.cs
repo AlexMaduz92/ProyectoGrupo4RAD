7d4b6640b4411604ee6c2188a166bb82d2a00b57
Kaschiel Urbina <kaschielhatake@gmail.com>
2024-04-16 20:14:38 -0600

    Codigo para el formulario de detallefactura.

--- Presentacion/PDestalleFactura.cs
+++ Presentacion/PDestalleFactura.cs
@@ -12,15 +12,227 @@ namespace Presentacion
 {
     public partial class PDestalleFactura : Form
     {
-        public PDestalleFactura()
+        public partial class PDestalleFactura : Form
         {
-            InitializeComponent();
-        }
+            public PDestalleFactura()
+            {
+                InitializeComponent();
+                UnitOfWork unitOfWork = new UnitOfWork();
+                BtnGuardar.Click += BtnGuardar_Click;
+                BtnModificar.Click += BtnModificar_Click;
+                BtnEliminar.Click += BtnEliminar_Click;
+            }
 
-        private void PDestalleFactura_Load(object sender, EventArgs e)
-        {
-            // TODO: esta línea de código carga datos en la tabla 'proyectoRadDataSet11.FacturaDetalles' Puede moverla o quitarla según sea necesario.
-            this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+            private int ObtenerProximoId()
+            {
+                int maxId = 0;
+                using (var context = new Exaconection())
+                {
+                    maxId = context.Set<FacturaDetalle>().Max(g => g.FacturaId);
+                }
+                return maxId + 1;
+            }
+
+            private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = DGVFacturaDetalle.Rows[e.RowIndex];
+                    TxtID.Text = row.Cells["FacturaDetalleIdDataGridViewTextBoxColumn"].Value.ToString();
+                    DTFCreacion.Value = Convert.ToDateTime(row.Cells["FechaCreacionDataGridViewTextBoxColumn"].Value);
+                    TxtTotal.Text = row.Cells["TotalDataGridViewTextBoxColumn"].Value.ToString();
+                    TxtSubTotal.Text = row.Cells["SubtotalDataGridViewTextBoxColumn"].Value.ToString();
+                    TxtDescuento.Text = row.Cells["DescuentoDataGridViewTextBoxColumn"].Value.ToString();
+                    TxtPrecio.Text = row.Cells["PrecioDataGridViewTextBoxColumn"].Value.ToString();
+                }
+            }
+
+            void limpiar()
+            {
+                TxtSubTotal.Text = "";
+                TxtDescuento.Text = "";
+                TxtTotal.Text = "";
+                TxtPrecio.Text = "";
+            }
+
+            private void ActualizarDataGridView()
+            {
+                // Actualizar el datadiv
+                this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+            }
+
+            private void BtnGuardar_Click(object sender, EventArgs e)
+            {
+                // Obtener los datos del formulario
+                string subtotal = TxtSubTotal.Text;
+                string descuento = TxtDescuento.Text;
+                string total = TxtTotal.Text;
+                string precio = TxtPrecio.Text;
+
+
+                DateTime fechaCreacion = DTFCreacion.Value;
+
+                // mensaje de confirmacion
+                DialogResult result = MessageBox.Show("¿Esta seguro?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+                if (result == DialogResult.Yes)
+                {
+                    if (string.IsNullOrWhiteSpace(subtotal) && string.IsNullOrWhiteSpace(total))
+                    {
+                        MessageBox.Show("Debe completar todos los campos.", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    FacturaDetalle facturaDetalless = new FacturaDetalle
+                    {
+                        SubTotal = decimal.Parse(subtotal),
+                        Descuento = decimal.Parse(descuento),
+                        Total = decimal.Parse(total),
+                        Precio = decimal.Parse(precio),
+                        FacturaId = int.Parse(CBXCodFactura.SelectedValue.ToString()),
+                        ProductoId = int.Parse(CBXCodProducto.SelectedValue.ToString()),
+                        FechaCreacion = fechaCreacion,
+                    };
+
+                    try
+                    {
+                        //Guardar en la db
+                        UnitOfWork.Repository<Factura>().Agregar(facturaDetalless);
+                        UnitOfWork.Guardar();
+
+                        // Actualizar el datadiv
+                        this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al guardar los detalles de la factura: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                ActualizarDataGridView();
+                limpiar();
+
+            }
+
+            private void PDestalleFactura_Load(object sender, EventArgs e)
+            {
+                // Obtener el próximo ID al guardar
+                int proximoId = ObtenerProximoId();
+
+                // Mostrar el próximo ID en el TxtID
+                TxtID.Text = proximoId.ToString();
+
+                // Cargar datos al datadiv
+                this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+
+                // Asignar el evento CellDoubleClick al DataGridView
+                DGVFacturaDetalle.CellContentDoubleClick += DGVFacturaDetalle_CellContentDoubleClick;
+
+                // Crear un DataView y aplicar un filtro para mostrar solo los registros con Estado en true
+                DataView dv = new DataView(this.proyectoRadDataSet11.FacturaDetalles);
+                dv.RowFilter = "Estado = true";
+
+                // Asignar el DataView filtrado al DataGridView
+                DGVFacturaDetalle.DataSource = dv;
+
+            }
+
+            private void BtnEliminar_Click(object sender, EventArgs e)
+            {
+                BtnModificar.Enabled = false;
+                BtnGuardar.Enabled = false;
+                // Obtener el ID del cliente a eliminar
+                int id = int.Parse(TxtID.Text);
+
+                // Mensaje de confirmación
+                DialogResult result = MessageBox.Show("¿Desea eliminar la factura?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        // Actualizar el Estado del descuento a 0
+                        FacturaDetalle facturass = UnitOfWork.Repository<Factura>().ObtenerPorId(id);
+                        facturass.Estado = false; // 0 representa falso
+
+                        //Guardar los cambios en la base de datos
+                        UnitOfWork.Repository<FacturaDetalle>().Editar(facturaDetalless);
+                        UnitOfWork.Guardar();
+
+                        // Actualizar el DataGridView
+                        this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al eliminar: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                ActualizarDataGridView();
+                limpiar();
+            }
+
+            private void BtnModificar_Click(object sender, EventArgs e)
+            {
+                // Obtener los datos del formulario
+                int id = int.Parse(TxtID.Text);
+                decimal subtotal = decimal.Parse(TxtSubTotal.Text);
+                decimal descuento = decimal.Parse(TxtDescuento.Text);
+                decimal total = decimal.Parse(TxtTotal.Text);
+                decimal precio = decimal.Parse(TxtPrecio.Text);
+
+                DateTime fechaCreacion = DTFCreacion.Value;
+
+                try
+                {
+                    // Obtener el GrupoDescuento existente desde el contexto
+                    FacturaDetalle facturaDetalless = UnitOfWork.Repository<FacturaDetalle>().ObtenerPorId(id);
+
+                    // Modificar las propiedades del objeto existente
+                    facturaDetalless.SubTotal = subtotal;
+                    facturaDetalless.Descuento = descuento;
+                    facturaDetalless.Total = total;
+                    facturaDetalless.Precio = precio;
+
+                    facturaDetalless.FechaCreacion = fechaCreacion;
+
+                    // Mensaje de confirmación
+                    DialogResult result = MessageBox.Show("¿Desea modificar la factura?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        // Guardar los cambios en la base de datos
+                        UnitOfWork.Guardar();
+
+                        // Actualizar el datadiv
+                        this.facturaDetallesTableAdapter.Fill(this.proyectoRadDataSet11.FacturaDetalles);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al modificar: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                limpiar();
+            }
+
+            private void DGVFacturaDetalle_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+            {
+                throw new NotImplementedException();
+            }
+
+            private void DGVFacturaDetalle_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+            {
+                throw new NotImplementedException();
+            }
+
+            private void DGVFacturaDetalle_Click(object sender, EventArgs e)
+            {
+                throw new NotImplementedException();
+            }
+
+            private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+            {
+
+            }
+
 
         }
     }
