e712fb7e8a108c63e6becf1393255db77531a20c
Kaschiel Urbina <kaschielhatake@gmail.com>
2024-04-16 20:17:39 -0600

    Kaschiel: Codigo para la clase Factura en la capa de negocio.

---
+++ Negocio/Factura.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Factura
+    {
+
+        private readonly Exaconection _dbContext;
+
+        public Factura()
+        {
+            _dbContext = new Exaconection();
+        }
+
+        public void GuardarFactura(Factura factura)
+        {
+            _dbContext.Facturas.Add(factura);
+            _dbContext.SaveChanges();
+
+        }
+
+        public void ModificarFactura(Factura factura)
+        {
+            var facturaExistente = _dbContext.Facturas.Find(factura.FacturaId);
+            if (facturaExistente != null)
+            {
+                facturaExistente.ClienteId = factura.ClienteId;
+                facturaExistente.PedidoId = factura.PedidoId;
+                facturaExistente.FechaCreacion = factura.FechaCreacion;
+                facturaExistente.FechaFactura = factura.FechaFactura;
+                facturaExistente.Estado = factura.Estado;
+                facturaExistente.Total = factura.Total;
+                facturaExistente.Subtotal = factura.Subtotal;
+                facturaExistente.Descuento = factura.Descuento;
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public void EliminarfACTURA(int FacturaId)
+        {
+            var factura = _dbContext.Pedidos.Find(FacturaId);
+            if (factura != null)
+            {
+                _dbContext.Pedidos.Remove(factura);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public List<int> ObtenerIdFacturas()
+        {
+            using (var dbContext = new Exaconection())
+            {
+                return dbContext.Facturas.Select(c => c.FacturaId).ToList();
+            }
+        }
+
+        public List<int> ObtenerIdClientes()
+        {
+            using (var dbContext = new Exaconection())
+            {
+                return dbContext.Clientes.Select(c => c.ClienteId).ToList();
+            }
+        }
+
+        public List<int> ObtenerIdPedidos()
+        {
+            using (var dbContext = new Exaconection())
+            {
+                return dbContext.Pedidos.Select(c => c.PedidoId).ToList();
+            }
+        }
+
+        public string ObtenerNombreClientePorId(int clienteId)
+        {
+            return _dbContext.Clientes.FirstOrDefault(c => c.ClienteId == clienteId)?.Nombres;
+        }
+    }
+}
