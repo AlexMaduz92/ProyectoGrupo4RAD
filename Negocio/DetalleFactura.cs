e8f46bd1ddb34d99605f7a9c24af5463be2e7a2c
Kaschiel Urbina <kaschielhatake@gmail.com>
2024-04-16 20:19:06 -0600

    Kaschiel: Codigo para la clase DetalleFactura en la capa Negocio.

---
+++ Negocio/DetalleFactura.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class FacturaDetalle
+    {
+        public readonly Exaconection _dbContext;
+
+        public FacturaDetalle()
+        {
+            _dbContext = new Exaconection();
+        }
+
+        public void GuardarFacturaDetalle(FacturaDetalle facturaDetalle)
+        {
+            _dbContext.Pedidos.Add(facturaDetalle);
+            _dbContext.SaveChanges();
+        }
+
+        public void ModificarFacturaDetalle(FacturaDetalle facturaDetalle)
+        {
+            var facturaDetalleExistente = _dbContext.FacturaDetalles.Find(facturaDetalleExistente.FacturaDetalleId);
+            if (facturaDetalle != null)
+            {
+                facturaDetalleExistente.FacturaId = facturaDetalle.facturaId;
+                facturaDetalleExistente.FechaCreacion = facturaDetalle.FechaCreacion;
+                facturaDetalleExistente.ProductoId = facturaDetalle.ProductoId;
+                facturaDetalleExistente.Precio = facturaDetalle.Precio;
+                facturaDetalleExistente.Total = facturaDetalle.Total;
+                facturaDetalleExistente.SubTotal = facturaDetalle.Subtotal;
+                facturaDetalleExistente.Descuento = facturaDetalle.Descuento;
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public void EliminarFacturaDetalle(int FacturaDetalleId)
+        {
+            var facturaDetalle = _dbContext.FacturaDetalles.Find(FacturaDetalleId);
+            if (facturaDetalle != null)
+            {
+                _dbContext.Pedidos.Remove(facturaDetalle);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public Pedido ObtenerFacturaPorId(int FacturaId)
+        {
+            using (var context = new Exaconection())
+            {
+                return context.FacturaDetalles.FirstOrDefault(p => p.FacturaDetalleId == facturaDetalleId);
+            }
+        }
+    }
+}
